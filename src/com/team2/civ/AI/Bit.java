31f85d849241df81b3c597bae0e727a910b7a3c3
Chase Stevens <chase@chasestevens.com>
2012-02-10 13:28:33 +0000

    implementing bit class

--- src/com/team2/civ/AI/Bit.java
+++ src/com/team2/civ/AI/Bit.java
@@ -1,1 +1,27 @@
-public class Bit extends Boolean {}
+public class Bit {
+	int value = 0;
+	Bit(boolean input){
+		if (input){
+			value = 1;
+		} else {
+			value = 0;
+		}
+	}
+	Bit(int input){
+		if (input == 0){
+			value = 0;
+		} else {
+			value = 1;
+		}
+	}		
+	public static int val(){
+		return value;
+	}
+	public static boolean truth(){
+		if (value == 1){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
