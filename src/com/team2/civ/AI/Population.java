94d0f75a83c312ecd65debef290c394dadb8c5b5
s1134468 <jerseyboy@hotmail.de>
2012-03-23 16:34:58 +0000

    bug fix: imports

--- src/com/team2/civ/AI/Population.java
+++ src/com/team2/civ/AI/Population.java
@@ -1,9 +1,19 @@
 package com.team2.civ.AI;
+import java.io.File;
+import java.io.IOException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
-import com.team2.civ.Game.GameController;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Population {
 	private int population_size;
