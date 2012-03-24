38cd854ccff3afdfae2ec53735444e598085c14e
H. Chase Stevens <chase@chasestevens.com>
2012-03-24 10:01:08 +0000

    Added generator support for resuming from saved population

--- src/com/team2/civ/AI/Generator.java
+++ src/com/team2/civ/AI/Generator.java
@@ -1,23 +1,45 @@
 package com.team2.civ.AI;
 
 import java.awt.GraphicsConfiguration;
+import java.io.File;
 
 public class Generator {
 	private int init_generations;
 	private Population pop;
-	
-	public Generator(int generations, int population_size, GraphicsConfiguration graphics){
+	private final boolean resume;
+
+	public Generator(int generations, int population_size) {
 		init_generations = generations;
 		pop = new Population(population_size);
+		resume = false;
 	}
-	
-	public void generate(){
-		pop.populate();
-		for (int i = 0; i < init_generations; i++){
-			pop.competeAll();
-			pop.cullHerd();
+
+	public Generator(File f) {
+		pop = new Population(f);
+		resume = true;
+	}
+
+	public void generate() {
+		if (resume) {
+			int current_generation = pop.getInitGeneration();
+			while (true) {
+				pop.competeAll();
+				pop.cullHerd();
+				current_generation++;
+				pop.writeAIs();
+				pop.writeMetadata(current_generation);
+			}
+		} else {
+			pop.populate();
+			for (int i = 0; i < init_generations; i++) {
+				pop.competeAll();
+				pop.cullHerd();
+				pop.writeAIs();
+				pop.writeMetadata(i);
+			}
+			pop.writeAIs();
+			pop.writeMetadata(init_generations);
 		}
-		pop.writeAIs();
 	}
 
 }
